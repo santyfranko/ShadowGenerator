5e1ce216d0202142c13c174c4f533a20bfdd2366
shivam gupta <frankoslc2069@gmail.com>
2020-09-30 19:38:16 +0545

    Updates

---
+++ precache-manifest.1e8d4ede3166b9d7c938cf1a7ce7c663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6094f903443e34496ca334f90163926f",
+    "url": "/ShadowGenerator/index.html"
+  },
+  {
+    "revision": "ca6af870cb895078a668",
+    "url": "/ShadowGenerator/static/css/main.8198c0db.chunk.css"
+  },
+  {
+    "revision": "7d5ca11537356f940dcf",
+    "url": "/ShadowGenerator/static/js/2.8920c66f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ShadowGenerator/static/js/2.8920c66f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca6af870cb895078a668",
+    "url": "/ShadowGenerator/static/js/main.4b89c379.chunk.js"
+  },
+  {
+    "revision": "fb785e8ddf9e108026da",
+    "url": "/ShadowGenerator/static/js/runtime-main.6a6906f1.js"
+  }
+]);
